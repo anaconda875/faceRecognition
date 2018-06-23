8712e1fee4f115cb3505f7cdb885b8d6a62b1f0b
root <root@DESKTOP-VV9F0RJ.localdomain>
2018-06-23 14:00:20 +0700

    re init

---
+++ app/src/main/java/com/example/nngbao/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.nngbao.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private JavaCameraView javaCameraView;
+    private Mat rgba;
+
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if(status == BaseLoaderCallback.SUCCESS)
+                javaCameraView.enableView();
+            else
+                super.onManagerConnected(status);
+        }
+    };
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        rgba = new Mat(width, height, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        rgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        rgba = inputFrame.rgba();
+        return rgba;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        javaCameraView = findViewById(R.id.camera);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(OpenCVLoader.initDebug())
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        else
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, baseLoaderCallback);
+    }
+}
